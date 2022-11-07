db3a68921b4272528e4fc01221f4c8d25ebab90d
Frede <fvilhelmsen@Fredes-MacBook-Pro.local>
2022-11-07 17:27:45 +0100

    Up to date

---
+++ 5._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Date from "./components/Date.svelte";
+
+  const name = "Anders";
+
+</script>
+
+
+<h2 id="page-title">First Svelte Project</h2>
+<div>More content for {name}.</div>
+<Date></Date>
+
+
+<style>
+  #page-title {
+    color: blue;
+  }
+</style>
